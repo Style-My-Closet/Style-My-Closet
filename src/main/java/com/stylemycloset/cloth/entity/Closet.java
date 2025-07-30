341a5b1b58adba4dcbb073988e2e1fbefcbe0859
양찬혁 <20184415@s.scnu.ac.kr>
2025-07-30 15:12:35 +0900

    re:base entity 상속하도록 (cloth)엔티티들 수정

--- src/main/java/com/stylemycloset/cloth/entity/Closet.java
+++ src/main/java/com/stylemycloset/cloth/entity/Closet.java
@@ -1,22 +1,32 @@
-//package com.stylemycloset.cloth.entity;
-//
-//@Entity
-//@Table(name = "closets")
-//@Getter
-//@NoArgsConstructor
-//public class Closet {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long closetId;
-//
-//  @OneToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name = "user_id", nullable = false)
-//  private User user;
-//
-//  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
-//  private offsetDatetime createdAt;
-//
-//
-//  @OneToMany(mappedBy = "closet", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//  private List<Cloth> clothes = new ArrayList<>();
-//}
+package com.stylemycloset.cloth.entity;
+
+import com.stylemycloset.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "closets")
+@Getter
+@NoArgsConstructor
+public class Closet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long closetId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime createdAt;
+
+
+    @OneToMany(mappedBy = "closet", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Cloth> clothes = new ArrayList<>();
+
+}
--- src/main/java/com/stylemycloset/cloth/entity/ClothingCategory.java
+++ src/main/java/com/stylemycloset/cloth/entity/ClothingCategory.java
@@ -1,19 +1,27 @@
-//package com.stylemycloset.cloth.entity;
-//
-//@Entity
-//@Table(name = "clothing_categories")
-//@Getter @Setter
-//@NoArgsConstructor
-//public class ClothingCategory {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  private Long categoryId;
-//
-//  @Column(nullable = false, length = 50)
-//  private String name;
-//
-//  @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
-//  private List<Cloth> clothes = new ArrayList<>();
-//
-//}
+package com.stylemycloset.cloth.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clothing_categories")
+@Getter
+@NoArgsConstructor
+public class ClothingCategory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long categoryId;
+
+  @Column(nullable = false, length = 50)
+  private String name;
+
+  @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+  @JoinColumn(name = "cloth_id",nullable = false)
+  private List<Cloth> clothes = new ArrayList<>();
+
+}
