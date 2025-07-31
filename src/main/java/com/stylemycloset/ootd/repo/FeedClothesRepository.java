4c84a5f23ead67f5f6847c91729b23df5cc6abe2
Jaeseok Heo <jasonheo1998@gmail.com>
2025-07-31 10:39:06 +0900

    feat: OOTD 피드 등록을 위한 Repo 생성

---
+++ src/main/java/com/stylemycloset/ootd/repo/FeedClothesRepository.java
@@ -0,0 +1,7 @@
+package com.stylemycloset.ootd.repo;
+
+import com.stylemycloset.ootd.entity.FeedClothes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedClothesRepository extends JpaRepository<FeedClothes, Long> {
+}
