da371132afb692dca382d6aa1624fc9b11b21c93
seonhyelin <seonseon933@gmail.com>
2025-08-01 09:28:05 +0900

    Refactor: IntegrationTestSupport 상속 구조로 변경

--- src/test/java/com/stylemycloset/sse/SseControllerTest.java
+++ src/test/java/com/stylemycloset/sse/SseControllerTest.java
@@ -4,6 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.stylemycloset.testutil.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,12 +16,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SseControllerTest {
+public class SseControllerTest extends IntegrationTestSupport {
 
   @Autowired
   MockMvc mockMvc;
 
 
+  @DisplayName("SSE 연결 요청을 보내면 SSE 연결 응답을 반환한다")
   @WithMockUser(username = "testuser", roles = "USER")
   @Test
   public void sseConnect_returnSseEmitter() throws Exception {
