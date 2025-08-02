d27a777c9d6139164f194a196d917256674e45c6
seonhyelin <seonseon933@gmail.com>
2025-08-02 17:45:54 +0900

    Test: 알림 테스트 코드 구현

---
+++ src/test/java/com/stylemycloset/notification/controller/NotificationControllerTest.java
@@ -0,0 +1,82 @@
+package com.stylemycloset.notification.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.stylemycloset.notification.entity.Notification;
+import com.stylemycloset.notification.entity.NotificationLevel;
+import com.stylemycloset.notification.repository.NotificationRepository;
+import com.stylemycloset.testutil.IntegrationTestSupport;
+import com.stylemycloset.user.dto.UserCreateRequest;
+import com.stylemycloset.user.entity.User;
+import com.stylemycloset.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class NotificationControllerTest extends IntegrationTestSupport {
+
+  private static final Logger log = LoggerFactory.getLogger(NotificationControllerTest.class);
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  NotificationRepository notificationRepository;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @DisplayName("알림 삭제 요청을 보낼 시 204응답이 온다")
+  @Test
+  void deleteNotifications_ReturnIsNoContent() throws Exception{
+    // given
+    UserCreateRequest request1 = new UserCreateRequest("test", "test@test.test", "test");
+    User user1 = new User(request1);
+    ReflectionTestUtils.setField(user1, "id", 1L);
+
+    Notification notification = notificationRepository.save(
+        new Notification(user1, "testTitle", "testContent", NotificationLevel.INFO));
+
+    // when & then
+    mockMvc.perform(
+        delete("/api/notifications/{receiverId}/{notificationId}",
+            user1.getId(), notification.getId()))
+        .andExpect(status().isNoContent());
+  }
+
+  @DisplayName("알림 조회 요청을 보낼 시 NotificationDtoCursorResponse가 응답으로 온다")
+  @Test
+  void findAllByReceiverId_shouldReturnNotificationDtoCursorResponse() throws Exception{
+    // given
+    UserCreateRequest request4 = new UserCreateRequest("test", "test@test.test", "test");
+    User user4 = new User(request4);
+    userRepository.save(user4);
+
+    notificationRepository.save(
+        new Notification(user4, "testTitle", "testContent", NotificationLevel.INFO));
+    notificationRepository.save(
+        new Notification(user4, "testTitle", "testContent", NotificationLevel.INFO));
+    notificationRepository.save(
+        new Notification(user4, "testTitle", "testContent", NotificationLevel.INFO));
+
+    notificationRepository.flush();
+
+    // when & then
+    mockMvc.perform(
+        get("/api/notifications/{userId}", user4.getId())
+            .param("limit", String.valueOf(2))
+    ).andExpect(status().isOk())
+        .andExpect(jsonPath("$.hasNext").value(true))
+        .andExpect(jsonPath("$.totalCount").value(3));
+  }
+}
