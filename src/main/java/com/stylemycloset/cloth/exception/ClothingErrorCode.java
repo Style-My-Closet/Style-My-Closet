c5f7889c31cc379ec6d6120d5cd97c09460be55c
양찬혁 <20184415@s.scnu.ac.kr>
2025-08-05 16:07:42 +0900

    chore: 자잘한 수정들

--- src/main/java/com/stylemycloset/cloth/exception/ClothingErrorCode.java
+++ src/main/java/com/stylemycloset/cloth/exception/ClothingErrorCode.java
@@ -7,6 +7,8 @@ public enum ClothingErrorCode {
     CLOTH_NOT_FOUND("CLOTH_001", "해당 의상을 찾을 수 없습니다."),
     CLOTH_LIST_FETCH_FAILED("CLOTH_002", "옷 목록 조회에 실패했습니다."),
     INVALID_ATTRIBUTE("ATTR_001", "유효하지 않은 속성입니다."),
+    ATTRIBUTE_DUPLICATE("ATTR_002", "이미 존재하는 속성입니다."),
+    ATTRIBUTE_NOT_FOUND("ATTR_003", "해당 속성을 찾을 수 없습니다."),
     INVALID_PARAMETER("PARAM_001", "잘못된 매개변수입니다."),
     CLOSET_NOT_FOUND("CLOSET_001", "해당 옷장을 찾을 수 없습니다."),
     CATEGORY_NOT_FOUND("CATEGORY_001", "해당 카테고리를 찾을 수 없습니다."),
