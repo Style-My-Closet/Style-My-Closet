7c476a02832b8c967affb7d7cb89f50af76c0240
Jaeseok Heo <jasonheo1998@gmail.com>
2025-08-07 11:37:41 +0900

    feat: FeeLike 레포 추가

---
+++ src/main/java/com/stylemycloset/ootd/repo/FeedLikeRepository.java
@@ -0,0 +1,18 @@
+package com.stylemycloset.ootd.repo;
+
+import com.stylemycloset.ootd.entity.Feed;
+import com.stylemycloset.ootd.entity.FeedLike;
+import com.stylemycloset.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
+
+  Optional<FeedLike> findByUserAndFeed(User user, Feed feed); // 유저와 피드로 좋아요 기록을 찾기 위한 메서드
+
+  long countByFeed(Feed feed);
+
+  boolean existsByUserAndFeed(User user, Feed feed);
+
+
+}
