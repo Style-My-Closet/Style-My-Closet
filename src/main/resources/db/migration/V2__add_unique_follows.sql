4754e62fddb090c6b6705b1ee9fab3d9b5bef9a4
황지환 <hth99131@gmail.com>
2025-08-09 03:52:59 +0900

    chore: flyway 변경이력 분할

--- src/main/resources/db/migration/V2__add_unique_follows.sql
+++ src/main/resources/db/migration/V2__add_unique_follows.sql
@@ -3,6 +3,3 @@ ALTER TABLE follows
     ADD CONSTRAINT uq_follower_followee UNIQUE (follower_id, followee_id);
 
 
-------------- Add Profile Id to User -----------
-ALTER TABLE users
-    ADD COLUMN profile_id UUID;
