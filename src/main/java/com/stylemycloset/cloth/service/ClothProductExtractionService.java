25bd68e602fa7394a31ac0f4e1069672d65c4d21
양찬혁 <20184415@s.scnu.ac.kr>
2025-08-11 12:00:06 +0900

    feat: 링크로 받아오는 서비스 추가

--- src/main/java/com/stylemycloset/cloth/service/ClothProductExtractionService.java
+++ src/main/java/com/stylemycloset/cloth/service/ClothProductExtractionService.java
@@ -1,14 +1,7 @@
 package com.stylemycloset.cloth.service;
 
-import com.stylemycloset.cloth.dto.response.ClothExtractionResponseDto;
 import com.stylemycloset.cloth.dto.ClothResponseDto;
 
 public interface ClothProductExtractionService {
-    
-    ClothExtractionResponseDto extractProductInfoFromUrl(String productUrl);
-
-    // 컨트롤러 응답 DTO로 가공까지 포함한 서비스 메서드
-    ClothResponseDto buildClothResponse(String productUrl);
-    
-    boolean isValidProductUrl(String productUrl);
-} 
+    ClothResponseDto extractAndSave(String productUrl, Long userId);
+}
