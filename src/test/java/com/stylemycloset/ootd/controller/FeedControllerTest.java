40396832e3e5c581f0ca5ce0b8ec80a7b5adc41d
Jaeseok Heo <jasonheo1998@gmail.com>
2025-08-21 14:32:30 +0900

    fix: .with(user(principal)) 누락

--- src/test/java/com/stylemycloset/ootd/controller/FeedControllerTest.java
+++ src/test/java/com/stylemycloset/ootd/controller/FeedControllerTest.java
@@ -169,7 +169,8 @@ public class FeedControllerTest extends IntegrationTestSupport {
               .param("limit", "10")
               .param("sortBy", "createdAt")
               .param("sortDirection", "DESCENDING")
-              .with(csrf()))
+              .with(csrf())
+              .with(user(principal))) // 인증된 사용자 정보 추가
           .andDo(print())
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.data.length()").value(10))
