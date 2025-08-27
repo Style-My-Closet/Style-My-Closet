8ce49a3e6e8ac3cef958a2169506e7faad6d7e22
Jun Hyeok Lee <dlwns4194@gmail.com>
2025-08-27 11:10:16 +0900

    fix:  weather createdate del: date 트리거 삭제

--- src/main/resources/db/migration/V11__algorithm-fix.sql
+++ src/main/resources/db/migration/V11__algorithm-fix.sql
@@ -8,21 +8,6 @@ UPDATE weather
     SET created_date = created_at::date
     WHERE created_date IS NULL;
 
-CREATE OR REPLACE FUNCTION set_weather_dates()
-    RETURNS TRIGGER AS $$
-BEGIN
-    NEW.created_date := NEW.created_at::date;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER trigger_set_dates
-    BEFORE INSERT OR UPDATE ON weather
-    FOR EACH ROW
-EXECUTE FUNCTION set_created_date();
-
-
 -- unique constraint 생성
 ALTER TABLE weather
     ADD CONSTRAINT uq_weather_forecast_per_day UNIQUE (created_date, forecast_at, location_id);
