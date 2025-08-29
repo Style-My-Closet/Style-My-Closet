76eee88d7cfc38b2f79f02e8385a3b7fbd302c12
황지환 <hth99131@gmail.com>
2025-08-29 18:51:24 +0900

    chore: 날씨 API 호출 버그를 고치기위한 로그 작성

--- src/main/java/com/stylemycloset/weather/controller/WeatherController.java
+++ src/main/java/com/stylemycloset/weather/controller/WeatherController.java
@@ -2,18 +2,12 @@ package com.stylemycloset.weather.controller;
 
 import com.stylemycloset.weather.dto.WeatherAPILocation;
 import com.stylemycloset.weather.dto.WeatherDto;
-import com.stylemycloset.weather.service.KakaoApiService;
 import com.stylemycloset.weather.service.WeatherService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/weathers")
 @RequiredArgsConstructor
@@ -38,9 +33,13 @@ public class WeatherController {
       @RequestParam(name = "latitude") double latitude,
       @AuthenticationPrincipal(expression = "userId") Long userId
   ) {
-
+    log.info("[WeatherAPI] 요청 - userId: {}, 위도: {}, 경도: {}",
+        userId, latitude, longitude);
     List<WeatherDto> weathers = weatherService.getWeatherByCoordinates(latitude, longitude);
+    log.info("[WeatherAPI] 조회된 날씨 데이터: {}", weathers);
     weatherService.checkWeather(latitude, longitude, userId);
+
+    log.info("[WeatherAPI] 응답 - userId: {}, 데이터 개수: {}", userId, weathers.size());
     return ResponseEntity.ok(weathers);
   }
 
@@ -49,7 +48,14 @@ public class WeatherController {
       @RequestParam(name = "longitude") double longitude,
       @RequestParam(name = "latitude") double latitude
   ) {
+
+    log.info("[WeatherLocation] 요청 - 위도: {}, 경도: {}", latitude, longitude);
     WeatherAPILocation location = weatherService.getLocation(latitude, longitude);
+    log.info("[WeatherLocation] 응답 - 요청 위도: {}, 경도: {}, 반환 위치명: {}, 행정구역코드: {}",
+        latitude, longitude,
+        location.x(),
+        location.y());
+
     return ResponseEntity.ok(location);
   }
 }
